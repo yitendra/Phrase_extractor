7d879c482dccec4355e0921915a267935b88dfc0
YITENDRA KUMAR <kumar.yitendra@gmail.com>
2018-06-22 18:13:20 +0530

    Created mycorpus

--- test.py
+++ test.py
@@ -9,9 +9,12 @@ clf=pickle.load(f)
 #Loading eval file
 eval_data=open("eval_data.txt",'r')
 X=list()
+all_words=list()
 for lines in eval_data:
+    all_words+=(lines.strip('\n').split(' '))
     X.append(lines.strip('\n'))
 eval_data.close()
+all_words=list(set(all_words))
 
 #Vectorizing and converting to array: X_test
 vectorizer=CountVectorizer(max_features=1000)
@@ -26,6 +29,12 @@ file.write("sent\tlabel\n")
 for i in range(len(result)):
     file.write(X[i]+'\t'+result[i]+'\n')
 file.close()
+
+#getting english stopwords
+from nltk.corpus import stopwords
+mycorpus=list()
+for words in stopwords.words('english'):
+    mycorpus.append(words)
 
 #For found, extract result text
 def extract(index):
